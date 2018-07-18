4a49abcee22df0e4dda5ee9c545520ff1a634fa1
Niccolo Brogi <me@nbrogi.com>
2018-07-18 22:16:26 +0200

    Add API version in routes

--- routes/api.php
+++ routes/api.php
@@ -2,38 +2,34 @@
 
 use Illuminate\Http\Request;
 
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| is assigned the "api" middleware group. Enjoy building your API!
-|
-*/
 Route::group([
-    'namespace' => 'Auth',
-    'middleware' => 'api',
-    'prefix' => 'auth'
+    'prefix' => 'v1' // <-- specify API version here
 ], function () {
-    Route::post('login', 'AuthController@login');
-    Route::post('signup', 'AuthController@signup');
-    Route::get('signup/activate/{token}', 'AuthController@signupActivate');
+    /*--------------------------------------------------------------------------
+      Auth
+    --------------------------------------------------------------------------*/
     Route::group([
-        'middleware' => 'auth:api'
-    ], function() {
-        Route::get('logout', 'AuthController@logout');
-        Route::get('user', 'AuthController@user');
+        'namespace' => 'Auth',
+        'middleware' => 'api',
+        'prefix' => 'auth'
+    ], function () {
+        Route::post('login', 'AuthController@login');
+        Route::post('signup', 'AuthController@signup');
+        Route::get('signup/activate/{token}', 'AuthController@signupActivate');
+        Route::group([
+            'middleware' => 'auth:api'
+        ], function() {
+            Route::get('logout', 'AuthController@logout');
+            Route::get('user', 'AuthController@user');
+        });
     });
-});
-
-Route::group([
-    'namespace' => 'Auth',
-    'middleware' => 'api',
-    'prefix' => 'password'
-], function () {
-    Route::post('create', 'PasswordResetController@create');
-    Route::get('find/{token}', 'PasswordResetController@find');
-    Route::post('reset', 'PasswordResetController@reset');
+    Route::group([
+        'namespace' => 'Auth',
+        'middleware' => 'api',
+        'prefix' => 'password'
+    ], function () {
+        Route::post('create', 'PasswordResetController@create');
+        Route::get('find/{token}', 'PasswordResetController@find');
+        Route::post('reset', 'PasswordResetController@reset');
+    });
 });
