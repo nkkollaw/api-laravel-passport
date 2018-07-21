e3986f29384a8d50cd22ef4534fa48778c3bb4a3
Niccolo Brogi <me@nbrogi.com>
2018-07-21 17:38:02 +0200

    Disable password-related routes while figuring out naming

--- routes/api.php
+++ routes/api.php
@@ -25,13 +25,13 @@ Route::group([
         });
     });
 
-    Route::group([
-        'namespace' => 'Auth',
-        'middleware' => 'api',
-        'prefix' => 'password'
-    ], function () {
-        Route::post('create', 'PasswordResetController@create');
-        Route::get('find/{token}', 'PasswordResetController@find');
-        Route::post('reset', 'PasswordResetController@reset');
-    });
+    // Route::group([
+    //     'namespace' => 'Auth',
+    //     'middleware' => 'api',
+    //     'prefix' => 'password'
+    // ], function () {
+    //     Route::post('create', 'PasswordResetController@create');
+    //     Route::get('find/{token}', 'PasswordResetController@find');
+    //     Route::post('reset', 'PasswordResetController@reset');
+    // });
 });
